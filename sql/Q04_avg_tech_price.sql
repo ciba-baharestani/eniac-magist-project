0bee85799aa3c0b5ccb49fad398eeb54213f0469
ciba-baharestani <taherehbaharestani@gmail.com>
2026-08-13 22:30:40 +0200

    Create Q04_avg_tech_price.sql

---
+++ sql/Q04_avg_tech_price.sql
@@ -0,0 +1,11 @@
+SELECT ROUND(AVG(price), 2) AS avg_tech_price
+FROM order_items oi
+JOIN products p ON oi.product_id = p.product_id
+WHERE p.product_category_name IN (
+        'computers_accessories',
+        'electronics',
+        'audio',
+        'informatica_acessorios',
+        'pc_gamer',
+        'tablets_impressao_imagem'
+   );
